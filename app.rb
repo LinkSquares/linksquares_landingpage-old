42dd8aec5fc679f511623c70853b09a5c8c8f188
Carlos Lazo <carlos.jose.lazo@gmail.com>
2014-10-13 23:04:48 -0400

    Fixes FlatUI dependencies, removes Ruby Gemfile dependency, and separates out index JavaScript.

--- app.rb
+++ app.rb
@@ -1,10 +1,3 @@
-# Include:
-# => Authentication File
-# => Concerns | Helpers | Models
-# => Database Configuration
-# => Debug Tools
-# => Sinatra Libraries
-
 require 'sinatra'
 require 'sinatra/activerecord'
 
@@ -19,37 +12,23 @@ module AppName
   class App < Sinatra::Application
     register Sinatra::ActiveRecordExtension
 
-    # Configure Options
-    # ==> Set default paths for static content.
     configure do
       set :root, File.dirname(__FILE__)
       set :public_folder, 'public'
     end
 
-    # ==> Set global JavaScript files for project.
     set :javascripts, [:jquery]
 
-    # Filters
-    # => add route filters.
-
-    # Routes
-    # => define controller actions.
-
-    # ==> Render index page.
     get '/' do
+      js  'index'
       erb :index
     end
 
-    # Helpers
-    # => define helper methods.
-
     helpers do
       include Rack::Utils
-      
-      # ==> Capability to escape HTML.
+
       alias_method :h, :escape_html
 
-      # ==> Enable partials in all templates.
       def partial(file_name)
         erb file_name, :layout => false
       end
