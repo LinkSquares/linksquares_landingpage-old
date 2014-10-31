22cbffbbd0f5b35b22319f8acd2c661b33e6174d
Vishal Sunak <vishal@backupify.com>
2014-10-31 11:02:00 -0400

    Working with Select box

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,19 @@
 $(document).ready(function() {
   $( '#target' ).submit(function() {
     $.ajax({
-      type: "POST",
-      url: "https://docs.google.com/a/linksquares.com/forms/d/1BUM2Wa4vJItqCJpBzAb35uuqV5iVdNOGqYAWP7HgnuY/formResponse",
-      data: {
+       url: "https://docs.google.com/a/linksquares.com/forms/d/1BUM2Wa4vJItqCJpBzAb35uuqV5iVdNOGqYAWP7HgnuY/formResponse",
+       data: {
         'entry.2107324868': $('.form-control.firstname-field').val(),
         'entry.1274413636': $('.form-control.lastname-field').val(),
         'entry.928505418': $('.form-control.company-field').val(),
         'entry.303352934': $('.form-control.email-field').val(),
-        'entry.1960377743': $('.form-control.phone-field').val()
-      }
-    });
+        'entry.1960377743': $('.form-control.phone-field').val(),
+        'entry.1561108851': $('#select-challenge option:selected').text()
+      },
+      type: "POST",
+      success: function(data) { 
+        alert("post success!"); }
 
-  $('.form-group.has-success').css("visibility","visible");
+    });
   });
 });
