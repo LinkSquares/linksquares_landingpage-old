e4b424b56d30d22a5b1ee0059e7779d92f1a750a
thesunak <vishalsunak@gmail.com>
2014-11-02 12:50:29 -0500

    Fixes AJAX submit to Google Form.

--- public/js/index.js
+++ public/js/index.js
@@ -1,31 +1,40 @@
 $(document).ready(function() {
-  $( '#target' ).submit(function() {
-    var challengesChecked = $("#challenges-checkbox input:checked").length
-
-    if (challengesChecked == 0) {
-          alert ("Challenges is empty")
-
-    }
-
-    else { 
-
-    $.ajax({
-       url: "https://docs.google.com/a/linksquares.com/forms/d/1BUM2Wa4vJItqCJpBzAb35uuqV5iVdNOGqYAWP7HgnuY/formResponse",
-       data: {
-        'entry.2107324868': $('.form-control.firstname-field').val(),
-        'entry.1274413636': $('.form-control.lastname-field').val(),
-        'entry.928505418': $('.form-control.company-field').val(),
-        'entry.303352934': $('.form-control.email-field').val(),
-        'entry.1561108851': $('#select-challenge option:selected').text()
-      },
-      type: "POST",
-      success: function(data) { 
-        alert("post success!"); }
-
-    });
-  
+  function verifyCheckboxes() {
+    return $("#challenges-checkbox input:checked").length > 0;
   }
 
+  function collectCheckboxData() {
+    $("#challenges-checkbox input:checked").each(function(index,value){ console.log($(this).text()); 
 
+  }
+
+  function postToGoogle() {
+    var data = {
+        'entry.1990565088': $('.form-control.firstname-field').val(),
+        'entry.1194990171': $('.form-control.lastname-field').val(),
+        'entry.86532129'  : $('.form-control.company-field').val(),
+        'entry.1434316936': $('.form-control.email-field').val(),
+        'entry.1418496140': collectCheckboxData()
+      }
+
+      $.post("https://docs.google.com/a/linksquares.com/forms/d/12bhQUzEBk8L55nwZdPMxniYWI5wqPCD0nc5suHgPb8w/formResponse", data);
+      
+      // Modify the DOM to show success (e.g. show hidden div with success message).
+
+      $('.form-group.has-success').show();
+      $('.form-group.has-error').hide();
+
+      return false;
+  }
+
+  $('#target').submit(function(event) {
+    event.preventDefault();
+
+    if (verifyCheckboxes()) {
+      postToGoogle();
+    } else {
+      $('.form-group.has-success').hide();
+      $('.form-group.has-error').show();
+    }
   });
 });
