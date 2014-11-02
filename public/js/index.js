17f5cee853b2d45374178908f163ca61ebae640b
Vishal Sunak <ShowMeTheV-Money@Vishals-MacBook-Pro-3.local>
2014-11-02 09:30:03 -0500

    Challenges checkbox created with jquery validation

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
   $( '#target' ).submit(function() {
+    var challengesChecked = $("#challenges-checkbox input:checked").length
+
+    if (challengesChecked == 0) {
+          alert ("Challenges is empty")
+
+    }
+
+    else { 
+
     $.ajax({
        url: "https://docs.google.com/a/linksquares.com/forms/d/1BUM2Wa4vJItqCJpBzAb35uuqV5iVdNOGqYAWP7HgnuY/formResponse",
        data: {
@@ -7,7 +16,6 @@ $(document).ready(function() {
         'entry.1274413636': $('.form-control.lastname-field').val(),
         'entry.928505418': $('.form-control.company-field').val(),
         'entry.303352934': $('.form-control.email-field').val(),
-        'entry.1960377743': $('.form-control.phone-field').val(),
         'entry.1561108851': $('#select-challenge option:selected').text()
       },
       type: "POST",
@@ -15,5 +23,9 @@ $(document).ready(function() {
         alert("post success!"); }
 
     });
+  
+  }
+
+
   });
 });
