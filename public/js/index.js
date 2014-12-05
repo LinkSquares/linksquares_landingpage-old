85b8855a883e702ba39fb4d498cdb168d3d3eae5
Vishal Sunak <vishal@backupify.com>
2014-12-05 11:45:11 -0500

    Post working to Google Sheets

--- public/js/index.js
+++ public/js/index.js
@@ -1,74 +1,25 @@
 $(document).ready(function() {
 
-  $('.form-control.questions-textarea').click(function(event) {
-    $('.form-control.questions-textarea').empty();
-  });
-
-
-  //Verifies that Challenges question checkbox has atleast 1 checkbox checked
-  function verifyChallengesCheckbox() {
-    return $("#challenges-checkbox input:checked").length > 0;
-  }
-
-  //Collects the checked Challenges question checkboxes
-  function collectChallengesCheckboxData() {
-    var checkboxChallengesData = [];
-    
-    $("#challenges-checkbox > label.checkbox input:checked").each(function(index,value) {
-      checkboxChallengesData.push($(this).parent().text().trim());
-    });
-
-    return checkboxChallengesData.join();
-  }
-
-  //Verifies that Interest question checkbox has atleast 1 checkbox checked
-  function verifyInterestCheckbox() {
-    return $("#interested-checkbox input:checked").length > 0;
-  }
-  
-  //Collects the checked Interest question checkboxes
-  function collectInterestCheckboxData() {
-    var checkboxInterestData = [];
-    
-    $("#interested-checkbox > label.checkbox input:checked").each(function(index,value) {
-      checkboxInterestData.push($(this).parent().text().trim());
-    });
-
-    return checkboxInterestData.join();
-  }
-
   function postToGoogle() {
     var data = {
-        'entry.1990565088': $('.form-control.firstname-field').val(),
-        'entry.1194990171': $('.form-control.lastname-field').val(),
-        'entry.86532129'  : $('.form-control.company-field').val(),
-        'entry.1434316936': $('.form-control.email-field').val(),
-        'entry.1418496140': collectChallengesCheckboxData(),
-        'entry.1648773653': collectInterestCheckboxData(),
-        'entry.1722671787': $('.form-control.questions-textarea').val()
+        'entry.921313200': $('.email-input').val()
       }
 
-      $.post("https://docs.google.com/a/linksquares.com/forms/d/12bhQUzEBk8L55nwZdPMxniYWI5wqPCD0nc5suHgPb8w/formResponse", data);
+      $.post("https://docs.google.com/a/linksquares.com/forms/d/19J9SD8mB8nqXDPIf6Ds1ECuuT09fFF5Tn7XdEd_UbEM/formResponse", data);
       
       // Modify the DOM to show success (e.g. show hidden div with success message).
 
-      $('.form-group.has-success').show();
-      $('.form-group.has-error').hide();
+      //$('.form-group.has-success').show();
+      //$('.form-group.has-error').hide();
 
       return false;
   }
 
   //When the form is submitted, validate the checkboxes are okay, then post to Google
-  $('#target').submit(function(event) {
+  $('#submit-button').click(function(event) {
     event.preventDefault();
-
-    if (verifyChallengesCheckbox() && verifyInterestCheckbox())  {
-      console.log("PASS")
+    console.log("PASS")
       postToGoogle();
-    } else {
-      console.log("FAIL")
-      $('.form-group.has-success').hide();
-      $('.form-group.has-error').show();
-    }
+
   });
 });
