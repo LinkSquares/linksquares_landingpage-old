ff4caf32967908732cef8363c609387d9d4a693a
Vishal Sunak <TheSunak@Vishals-MacBook-Pro.local>
2014-12-06 18:08:59 -0500

    Added 3 bullets below the H1

--- public/js/index.js
+++ public/js/index.js
@@ -6,24 +6,9 @@ $(document).ready(function() {
       }
 
       $.post("https://docs.google.com/a/linksquares.com/forms/d/19J9SD8mB8nqXDPIf6Ds1ECuuT09fFF5Tn7XdEd_UbEM/formResponse", data);
-      
-      // Modify the DOM to show success (e.g. show hidden div with success message).
-
-      //$('.form-group.has-success').show();
-      //$('.form-group.has-error').hide();
 
       return false;
   }
-
-  //When the form is submitted, validate the checkboxes are okay, then post to Google
-  
- // if validateEmail($('.form-control.email-field').val())
-
-  //else
-
-
-
-
 
   $('#target').submit(function(event) {
     event.preventDefault();
