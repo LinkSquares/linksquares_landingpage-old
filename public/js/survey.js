758d418fa2befc3f9dcd07c9f168605b5f01af43
Vishal Sunak <vishalsunak@gmail.com>
2015-03-21 17:44:22 -0400

    Created /survey page

--- public/js/survey.js
+++ public/js/survey.js
@@ -2,32 +2,21 @@ $(document).ready(function() {
 
   function postToGoogle() {
     var data = {
-        'entry.2134126697': $('.form-control.fullname-field').val(),
-        
+        'entry.27691755': $('#survey-dashboard').val(),
+        'entry.1028587106': $('#survey-square').val(),
+        'entry.46583915': $('#square-collapsed').val(),
+        'entry.1671733856': $('#square-expanded').val()
       }
 
-      $.post("https://docs.google.com/forms/d/1bjhxTMJfChy-Z0rjxTfuDhf09icjJukD7MpxQTDdVNI/formResponse", data);
+      $.post("https://docs.google.com/forms/d/13jvrZk9IMINWCvjFScT8a26GkvECaiIwpenCvEQ6ztM/formResponse", data);
 
       return false;
   }
 
   $('#target').submit(function(event) {
     event.preventDefault();
+     $('.thanks-text').show();
     postToGoogle();
 
   });
 });
-
-
-
-/*entry.2134126697:
-entry.575073340:Email
-entry.952718322:email service
-entry.1109339131:hardest part
-entry.2003739538:understanding 
-entry.241449062:save time
-entry.976363206:locate links and docs
-entry.877269600:more productive
-entry.1866511601:manage your work email
-entry.620199057:phone interview
-entry.1374350073:phone number*/
