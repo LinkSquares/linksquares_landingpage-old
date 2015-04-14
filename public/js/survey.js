f40a56dec7cb78011325bbee0cc403e0f10ae822
Vishal Sunak <vishal@backupify.com>
2015-04-14 18:38:35 -0400

    On /survey, added general feedback question and updated .js file. Also, removed the square collapsed UI question and updated the .js file for the POST

--- public/js/survey.js
+++ public/js/survey.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
         'entry.351153168': $('#fullname').val(),
         'entry.27691755': $('#survey-dashboard').val(),
         'entry.1028587106': $('#survey-square').val(),
-        'entry.46583915': $('#square-collapsed').val(),
-        'entry.1671733856': $('#square-expanded').val()
+        'entry.1671733856': $('#square-expanded').val(),
+        'entry.1878339956': $('#general').val()
       }
 
       $.post("https://docs.google.com/forms/d/13jvrZk9IMINWCvjFScT8a26GkvECaiIwpenCvEQ6ztM/formResponse", data);
